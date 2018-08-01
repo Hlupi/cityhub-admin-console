55c1942cfd5f38a7767bc494db126790e415a940
rbhgn <robhgn@gmail.com>
2018-08-01 22:38:01 +0200

    Fixed not working autostart on slider

--- src/components/slider/main/Carousel.js
+++ src/components/slider/main/Carousel.js
@@ -9,13 +9,15 @@ import { connect } from 'react-redux';
 import { fetchSliderData } from '../../../actions/sliderData'
 import { fetchHostData } from '../../../actions/host'
 
-class TestCarousel extends Component {
+    class TestCarousel extends Component {
+        state={autoPlay: true}
     param = this.props.match.params.location
+
 
     componentDidMount() {
         this.props.fetchSliderData(this.param)
         this.props.fetchHostData()
-        const timer = setInterval(() => window.location.reload(), 30*60000);
+        const timer = setInterval(() => window.location.reload(), 1000 * 120);
         this.setState({timer});
     }
 
@@ -24,11 +26,15 @@ class TestCarousel extends Component {
     }
 
     render() {
-        console.log(this.props.host)
+        
         return (
             <div>
-            <Carousel autoPlay interval={15000} infiniteLoop showThumbs={false} showIndicators={false} showStatus={false}>
-                {this.props.slider.map(item => {
+                    <Carousel autoPlay={setTimeout(() => this.state.autoPlay, 2000)} interval={1000} infiniteLoop showThumbs={false} showIndicators={false} showStatus={false} stopOnHover={false} swipeable={false}>
+            
+            
+                {
+                 
+                    this.props.slider.map(item => {
                     if (item.source === "instagram") {
                         return (
                             <div key={item}><Instagram data={item} /></div>

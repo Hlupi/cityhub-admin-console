ab7ce0b8735c6aa77b68a711e77949235317d9cf
Iuliia Ashikhmina <yu.ashikhmina@gmail.com>
2018-08-02 15:12:45 +0200

    added user in app

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Scraper from './components/dashboard/scraper/Scraper'
 class App extends Component {
 
   render() {
+    const { user } = this.props
     return (
       <Router>
         <div>
